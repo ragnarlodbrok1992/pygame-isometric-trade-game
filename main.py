606b709cb2b102653a5659c12d6c2663b4b6b854
ragnarlodbrok1992 <maraus92@gmail.com>
2025-01-24 22:09:09 +0100

    WIP

--- main.py
+++ main.py
@@ -8,36 +8,36 @@ pygame.init()
 
 # Main engine stuff
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption(WINDOW_TITLE)
+screen = pygame.display.set_mode((CONF_WINDOW_WIDTH, CONF_WINDOW_HEIGHT))
+pygame.display.set_caption(CONF_WINDOW_TITLE)
 
 # UI stuff
-debug_font = pygame.font.SysFont("Fira Code Medium", UI_FONT_SIZE, bold=True)
+debug_font = pygame.font.SysFont("Fira Code Medium", CONF_UI_FONT_SIZE, bold=True)
 text_color = (255, 255, 255)
 
 DEBUG = False
 
-while ENGINE_RUNNING:
+while CNTRL_ENGINE_RUNNING:
     # Event handling
     for event in pygame.event.get():
         # Check if quitting
         if event.type == pygame.QUIT:
-            ENGINE_RUNNING = False
+            CNTRL_ENGINE_RUNNING = False
 
         # Check for keyboard input
         if event.type == pygame.KEYDOWN:
             # Check for Q - quit the game
             if event.key == pygame.K_q:
-                ENGINE_RUNNING = False
+                CNTRL_ENGINE_RUNNING = False
             # Turning debug on and off
             if pygame.key.get_mods() & pygame.KMOD_LCTRL and event.key == pygame.K_F11:
                 DEBUG = not DEBUG
 
     # Calculate delta time
-    dt = clock.tick(FPS) / 1000.0
+    dt = clock.tick(CONF_FPS) / 1000.0
 
     # Fill screen with the background color
-    screen.fill(BACKGROUND_COLOR)
+    screen.fill(CONF_BACKGROUND_COLOR)
 
     # Render stuff
     # UI stuff
@@ -48,7 +48,7 @@ while ENGINE_RUNNING:
         screen.blit(dt_text, (10, 10))
 
     # Update FPS counter in window title
-    pygame.display.set_caption(f"{WINDOW_TITLE} FPS: {clock.get_fps(): .2f}")
+    pygame.display.set_caption(f"{CONF_WINDOW_TITLE} FPS: {clock.get_fps(): .2f}")
 
     # Update the display
     pygame.display.flip()
