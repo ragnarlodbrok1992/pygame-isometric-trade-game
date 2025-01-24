20fff0c4eee3f300435a62b9a6e14f8b0798c1d3
ragnarlodbrok1992 <maraus92@gmail.com>
2025-01-24 21:49:31 +0100

    WIP

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ pygame.display.set_caption(WINDOW_TITLE)
 debug_font = pygame.font.SysFont("Fira Code Medium", UI_FONT_SIZE, bold=True)
 text_color = (255, 255, 255)
 
+DEBUG = False
+
 while ENGINE_RUNNING:
     # Event handling
     for event in pygame.event.get():
@@ -27,6 +29,9 @@ while ENGINE_RUNNING:
             # Check for Q - quit the game
             if event.key == pygame.K_q:
                 ENGINE_RUNNING = False
+            # Turning debug on and off
+            if pygame.key.get_mods() & pygame.KMOD_LCTRL and event.key == pygame.K_F11:
+                DEBUG = not DEBUG
 
     # Calculate delta time
     dt = clock.tick(FPS) / 1000.0
@@ -36,9 +41,11 @@ while ENGINE_RUNNING:
 
     # Render stuff
     # UI stuff
-    dt_text = debug_font.render(f"Delta Time: {dt: .4f} sec", True, text_color)
 
-    screen.blit(dt_text, (10, 10))
+    # Debug text
+    if DEBUG:
+        dt_text = debug_font.render(f"Delta Time: {dt: .4f} sec", True, text_color)
+        screen.blit(dt_text, (10, 10))
 
     # Update FPS counter in window title
     pygame.display.set_caption(f"{WINDOW_TITLE} FPS: {clock.get_fps(): .2f}")
