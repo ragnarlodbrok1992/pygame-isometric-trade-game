f6c82f58ddf5808332911581cedf6a633f82e709
ragnarlodbrok1992 <maraus92@gmail.com>
2025-02-21 12:32:32 +0100

    WIP

---
+++ build.bat
@@ -0,0 +1,9 @@
+@echo off
+SET compiler="C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\14.42.34433\bin\Hostx64\x64\cl.exe"
+
+IF NOT EXIST build mkdir build
+
+pushd build
+
+popd
+
