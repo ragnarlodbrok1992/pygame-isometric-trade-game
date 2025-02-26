53d8d89008f955f940c6aa926182a9054211a1d0
ragnarlodbrok1992 <maraus92@gmail.com>
2025-02-26 21:58:40 +0100

    WIP

--- build.bat
+++ build.bat
@@ -1,9 +1,31 @@
 @echo off
 SET compiler="C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\14.42.34433\bin\Hostx64\x64\cl.exe"
 
+REM relative to build directory
+SET sdl_dir=..\c_isometric_engine\ext\sdl\
+SET sdl_build=..\c_isometric_engine\ext\sdl\VisualC\x64\Debug\
+SET sdl_include=include
+SET sdl_dll=SDL3.dll
+SET sdl_lib=SDL3.lib
+
 IF NOT EXIST build mkdir build
 
 pushd build
+
+echo Copying dll file...
+
+copy %sdl_build%%sdl_dll% .
+
+echo Compiling...
+
+%compiler% /EHsc /Zi^
+  /DEBUG:FULL^
+  /INCREMENT:NO^
+  /std:c++17^
+  /Fe:"isometric_trade_game"^
+  ../c_isometric_engine/main.cpp^
+  -I%sdl_dir%%sdl_include%^
+  /link /LIBPATH:%sdl_build% %sdl_lib%
 
 popd
 
--- c_isometric_engine/main.cpp
+++ c_isometric_engine/main.cpp
@@ -1,4 +1,48 @@
-int main(int argc, char* argv[]) {
-  
-  return 0;
+#define SDL_MAIN_USE_CALLBACKS 1
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+#include <stdio.h>
+
+static SDL_Window* window = nullptr;
+static SDL_Renderer* renderer = nullptr;
+
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
+  SDL_SetAppMetadata("SDL Isometric Trade Game", "alpha", "ragnar.engine.ISOMETRIC");
+
+  if (!SDL_Init(SDL_INIT_VIDEO)) {
+    SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+    return SDL_APP_FAILURE;
+  }
+
+  if (!SDL_CreateWindowAndRenderer("SDL Isometric Trade Game", 1280, 768, 0, &window, &renderer)) {
+    SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+    return SDL_APP_FAILURE;
+  }
+
+  return SDL_APP_CONTINUE; // This is crucial...
+}
+
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
+  if (event->type == SDL_EVENT_QUIT) {
+    return SDL_APP_SUCCESS;
+  }
+
+  // Exit on Q
+  if (event->type == SDL_EVENT_KEY_DOWN) {
+    // Something something printf
+    printf("Pressing buttons!\n");
+  }
+
+  // If everythingi is allright - proceed
+  return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void* appstate) {
+  // Runs once per frame
+  return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void* appstate, SDL_AppResult result) {
+  // Clean stuff here - if you want of course
 }
